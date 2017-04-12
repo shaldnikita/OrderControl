9b0fc71db1a312291d9a5917df6ea0fcf01d4897
ShaldNikita <shaldnikita2@yandex.ru>
2017-04-12 12:58:10 +0300

    Fixed work with db connection

--- src/main/java/ru/testwork/Connection.java
+++ src/main/java/ru/testwork/Connection.java
@@ -10,13 +10,19 @@ import javax.persistence.*;
  */
 public class Connection {
 
-    private EntityManagerFactory emf
-            = Persistence.createEntityManagerFactory("RemakingWebAppPU");
+    private EntityManagerFactory emf;
     private EntityManager em;
+    private EntityTransaction tx;
     private static Connection connection;
 
     static {
         connection = new Connection();
+    }
+
+    {
+        emf = Persistence.createEntityManagerFactory("RemakingWebAppPU");
+        em = emf.createEntityManager();
+        tx = em.getTransaction();
     }
 
     private Connection() {
@@ -24,92 +30,74 @@ public class Connection {
 
     public boolean addElementToDB(BouquetOrder p) throws Exception {
         try {
-            em = emf.createEntityManager();
-            em.getTransaction().begin();
+            tx.begin();
             em.persist(p);
-            em.getTransaction().commit();
+            tx.commit();
             return true;
         } catch (Exception e) {
             e.printStackTrace();
             em.getTransaction().rollback();
             return false;
-        } finally {
-            em.close();
         }
     }
 
     public BouquetOrder getElementFromDB(String id) throws Exception {
         BouquetOrder p = null;
-        try {
-            em = emf.createEntityManager();
-            p = em.find(BouquetOrder.class, id);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            em.close();
-        }
+        p = em.find(BouquetOrder.class, id);
         return p;
     }
 
-    public boolean updateField(Long id, Optional<String> wishes, Optional<Integer> moneySpent, Optional<Integer> total, Optional<String> path,Boolean isReady) throws Exception {
+    public boolean updateField(Long id, Optional<String> wishes, Optional<Integer> moneySpent, Optional<Integer> total, Optional<String> path, Boolean isReady) throws Exception {
+        BouquetOrder order = null;
         try {
-            em = emf.createEntityManager();
-            em.getTransaction().begin();
-            BouquetOrder order = em.find(BouquetOrder.class, id);
+            tx.begin();
+            order = em.getReference(BouquetOrder.class, id);
             order.setWishes(wishes.orElse(null));
             order.setMoneySpent(moneySpent.orElse(null));
             order.setTotal(total.orElse(null));
             order.setImgPath(path.orElse(null));
-            order.setStatus(isReady? Status.Ready : Status.Accepted);
-            em.getTransaction().commit();
+            order.setStatus(isReady ? Status.Ready : Status.Accepted);
+            tx.commit();
             return true;
         } catch (Exception e) {
             em.getTransaction().rollback();
             e.printStackTrace();
             return false;
-        } finally {
-            em.close();
         }
     }
 
     public boolean updateStatus(String id, Status status) {
+        BouquetOrder order;
         try {
-            em = emf.createEntityManager();
-            em.getTransaction().begin();
-            BouquetOrder order = em.find(BouquetOrder.class, id);
+            tx.begin();
+            order = em.getReference(BouquetOrder.class, id);
             order.setStatus(status);
-            em.getTransaction().commit();
+            tx.commit();
             return true;
         } catch (Exception e) {
             em.getTransaction().rollback();
             e.printStackTrace();
             return false;
-        } finally {
-            em.close();
         }
-        
     }
 
     public boolean removeOrder(Long id) throws Exception {
+        BouquetOrder order = null;
         try {
-            em = emf.createEntityManager();
-            em.getTransaction().begin();
-            BouquetOrder order = em.find(BouquetOrder.class, id);
+            tx.begin();
+            order = em.find(BouquetOrder.class, id);
             em.remove(order);
-            em.getTransaction().commit();
+            tx.commit();
             return true;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
-        } finally {
-            em.close();
         }
     }
 
     public ArrayList<BouquetOrder> getBouquetOrderList() {
         em = emf.createEntityManager();
         return new ArrayList(em.createNamedQuery("getBouquetOrderList", BouquetOrder.class).getResultList());
-
     }
 
     public static Connection getConnection() {
